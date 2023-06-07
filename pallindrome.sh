bbf475c49f2838e1d67cc6e6c288dff24e9ddaf7
niharpiscope <135088129+niharpiscope@users.noreply.github.com>
2023-06-07 09:14:34 +0530

    Update pallindrome.sh
    Added 3rd line of code

--- pallindrome.sh
+++ pallindrome.sh
@@ -2,3 +2,4 @@
 
 This is a program which is going to compare two words
 if they are same, then the word is a pallindrome
+I like to write a code which is going to check whether a number is a pallindrome or not
